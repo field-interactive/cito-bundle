f6bda504f75afd5b957594a05edfa7fffa780d32
Tim Kompernaß <t.kompernass@field-interactive.com>
2018-08-08 08:44:51 +0200

    fix mirror

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -48,7 +48,7 @@ class ScriptHandler
             }
         }
         if (!$citoBase) {
-            $fs->mirror(__DIR__ . '/../Resources/templates/', $templateDir . '/', true);
+            $fs->mirror(__DIR__ . '/../Resources/templates/', $templateDir . '/');
         }
 
         // Add public files
