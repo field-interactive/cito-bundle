ccacf59d276678922646240eaf33be06f59f6c7e
domideimel <32941053+domideimel@users.noreply.github.com>
2019-05-16 15:53:59 +0200

    Update setImageHeight.js
    fixed height calc

--- Resources/public/assets/js/utility/setImageHeight.js
+++ Resources/public/assets/js/utility/setImageHeight.js
@@ -1,13 +1,15 @@
 import { images } from '../vars';
-
 const setImageHeight = _ => {
     ;['load', 'resize'].forEach(event => {
         window.addEventListener(event, _ => {
             if (!images.length) return false;
             images.forEach(img => {
                 const ratio = img.getAttribute('data-ratio');
-                const width = img.offsetWidth;
-                img.style.height = `${Math.floor(width / ratio)}px`;
+                setTimeout(_  => {
+                    const width = img.offsetHeight;
+                    img.style.height = `${Math.floor(width / ratio)}px`;
+                }, 30)
+
             });
         });
     });
