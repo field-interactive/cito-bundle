1bf35af76bc85d84925b3a92bd0c0d8cbbed55de
dominikdeimel <d.deimel@field-interactive.com>
2019-05-17 09:32:38 +0200

    fixed issue with the image lazyloading

--- Resources/public/assets/js/app.js
+++ Resources/public/assets/js/app.js
@@ -1,8 +1,22 @@
 import jQuery from 'jquery';
-import { lazyload } from './modules/lazyload';
 import { cookies } from './modules/cookies';
+import { setImageHeight } from './utility/setImageHeight';
+
+(function (w, d) {
+    const b = d.getElementsByTagName('body')[0];
+    const s = d.createElement('script');
+    s.async = true;
+    s.defer = true;
+    const v = !('IntersectionObserver' in w) ? '8.7.1' : '12.0.0';
+    s.src = 'https://cdnjs.cloudflare.com/ajax/libs/vanilla-lazyload/' + v + '/lazyload.min.js';
+    w.lazyLoadOptions = [{
+        callback_loaded: el => {
+            setImageHeight(el);
+        }
+    }];
+    b.appendChild(s);
+})(window, document);
 
 jQuery(document).ready(_ => {
-    lazyload();
     cookies();
 });
