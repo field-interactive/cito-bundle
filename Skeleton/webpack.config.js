f9a181695b1ba80d72cd11a1e5bf0fc5ac05c6d0
dominikdeimel <d.deimel@field-interactive.com>
2019-05-17 14:04:02 +0200

    added slimmed build process

--- Skeleton/webpack.config.js
+++ Skeleton/webpack.config.js
@@ -58,6 +58,137 @@ if (Encore.isProduction()) {
             ],
             runtimeCaching: [
                 {
+                    urlPattern: new RegExp('https://cdnjs.cloudflare.com/'),
+                    handler: 'StaleWhileRevalidate'
+                }
+            ],
+            swDest: '../sw.js'
+        })
+    );
+    // Generate HTML for Favicons
+    Encore.addPlugin(
+        new HtmlWebpackPlugin({
+            filename: path.join(__dirname, 'templates/partials/favicons.html.twig'),
+            template: path.join(__dirname, 'templates/faviconsTmpl.html.twig'),
+            inject: true,
+            minify: false,
+            excludeChunks: ['css/main', 'js/main']
+        })
+    );
+    // Generate Favicons
+    Encore.addPlugin(
+        new WebappWebpackPlugin({
+            logo: path.join(__dirname, config.icon.path),
+            cache: true,
+            publicPath: '/build/icons',
+            outputPath: 'icons',
+            prefix: '',
+            inject: true,
+            orientation: 'portrait',
+            favicons: {
+                appName: null,
+                appDescription: null,
+                developerName: null,
+                developerURL: null,
+                theme_color: null,
+                lang: null,
+                start_url: null,
+                icons: {
+                    background: false,
+                    android: false,
+                    appleIcon: false,
+                    appleStartup: false,
+                    coast: false,
+                    favicons: true,
+                    firefox: false,
+                    opengraph: false,
+                    twitter: false,
+                    yandex: false,
+                    windows: false
+                }
+            }
+        })
+    );
+}
+
+const standardConfig = Encore.getWebpackConfig();
+
+standardConfig.name = 'standardConfig';
+standardConfig.watchOptions = {
+    poll: true,
+    ignored: /node_modules/
+};
+
+// Requires symfony server to be configured to serve on localhost:8000
+standardConfig.devServer = {
+    public: 'localhost:8000',
+    allowedHosts: ['localhost:8000'],
+    contentBase: path.join(__dirname, 'public/'),
+    watchContentBase: true,
+    compress: true,
+    open: true,
+    disableHostCheck: true,
+    progress: true,
+    watchOptions: {
+        watch: true,
+        poll: true
+    }
+};
+
+Encore.reset();
+
+Encore.setOutputPath(config.assetsPath)
+      .setPublicPath(config.publicPath)
+      .addEntry(config.scripts.frontend.dest, config.scripts.frontend.src)
+      .addStyleEntry(config.styles.frontend.dest, config.styles.frontend.src)
+      .enableSassLoader()
+      .enablePostCssLoader(options => {
+          options.config = {
+              path: './postcss.config.js'
+          };
+      })
+      .configureBabel(() => {}, {
+          useBuiltIns: 'usage',
+          corejs: 3
+      })
+      .splitEntryChunks()
+      .configureSplitChunks(splitChunks => {
+          splitChunks.minSize = 0;
+      })
+      .configureTerserPlugin(options => {
+          options.cache = true;
+          options.terserOptions = {
+              output: {
+                  comments: false
+              }
+          };
+      })
+      .disableSingleRuntimeChunk()
+      .autoProvidejQuery()
+      .cleanupOutputBeforeBuild()
+      .enableBuildNotifications()
+      .enableSourceMaps(!Encore.isProduction())
+      .enableVersioning(Encore.isProduction());
+
+if (Encore.isProduction()) {
+    // GZip Compression
+    Encore.addPlugin(
+        new CompressionPlugin({
+            test: /\.(js|css)$/,
+            cache: true
+        })
+    );
+    // SW Generation
+    Encore.addPlugin(
+        new WorkboxPlugin.GenerateSW({
+            globDirectory: 'public',
+            clientsClaim: true,
+            skipWaiting: true,
+            globPatterns: [
+                '**/*.{gz,js,css,jpg,JPG,png,ico,otf,eot,ttf,woff,woff2,svg}'
+            ],
+            runtimeCaching: [
+                {
                     urlPattern: new RegExp('/*(.*)'),
                     handler: 'StaleWhileRevalidate'
                 },
@@ -69,17 +200,17 @@ if (Encore.isProduction()) {
             swDest: '../sw.js'
         })
     );
-    // Add generated Icon links to Faviconstemplate
+    // Generate HTML for Favicons + PWA
     Encore.addPlugin(
         new HtmlWebpackPlugin({
             filename: path.join(__dirname, 'templates/partials/favicons.html.twig'),
             template: path.join(__dirname, 'templates/faviconsTmpl.html.twig'),
             inject: true,
             minify: false,
-            excludeChunks: [ 'css/main', 'js/main' ]
+            excludeChunks: ['css/main', 'js/main']
         })
     );
-    // Generate Favicons
+// Generate Favicons + PWA stuff
     Encore.addPlugin(
         new WebappWebpackPlugin({
             logo: path.join(__dirname, config.icon.path),
@@ -107,28 +238,7 @@ if (Encore.isProduction()) {
     );
 }
 
-const fullConfig = Encore.getWebpackConfig();
+const pwa = Encore.getWebpackConfig();
+pwa.name = 'pwa';
 
-fullConfig.name = 'full';
-fullConfig.watchOptions = {
-    poll: true,
-    ignored: /node_modules/
-};
-
-// Requires symfony server to be configured to serve on localhost:8000
-fullConfig.devServer = {
-    public: 'localhost:8000',
-    allowedHosts: ['localhost:8000'],
-    contentBase: path.join(__dirname, 'public/'),
-    watchContentBase: true,
-    compress: true,
-    open: true,
-    disableHostCheck: true,
-    progress: true,
-    watchOptions: {
-        watch: true,
-        poll: true
-    }
-};
-
-module.exports = fullConfig;
+module.exports = [standardConfig, pwa];
