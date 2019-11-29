97f9c7daabd8bd6fbb0f965fb73a1f69d3d7fa88
Tim Kompernaß <t.kompernass@field-interactive.com>
2019-11-29 14:07:51 +0100

    updated to s4.4

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -9,8 +9,8 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('cito');
+        $treeBuilder = new TreeBuilder('cito');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
--- Skeleton/webpack.config.js
+++ Skeleton/webpack.config.js
@@ -47,68 +47,6 @@ if (Encore.isProduction()) {
             cache: true
         })
     );
-    // SW Generation
-    Encore.addPlugin(
-        new WorkboxPlugin.GenerateSW({
-            globDirectory: 'public',
-            clientsClaim: true,
-            skipWaiting: true,
-            globPatterns: [
-                '**/*.{gz,js,css,jpg,JPG,png,ico,otf,eot,ttf,woff,woff2,svg}'
-            ],
-            runtimeCaching: [
-                {
-                    urlPattern: new RegExp('https://cdnjs.cloudflare.com/'),
-                    handler: 'StaleWhileRevalidate'
-                }
-            ],
-            swDest: '../sw.js'
-        })
-    );
-    // Generate HTML for Favicons
-    Encore.addPlugin(
-        new HtmlWebpackPlugin({
-            filename: path.join(__dirname, 'templates/partials/favicons.html.twig'),
-            template: path.join(__dirname, 'templates/faviconsTmpl.html.twig'),
-            inject: true,
-            minify: false,
-            excludeChunks: ['css/main', 'js/main']
-        })
-    );
-    // Generate Favicons
-    Encore.addPlugin(
-        new WebappWebpackPlugin({
-            logo: path.join(__dirname, config.icon.path),
-            cache: true,
-            publicPath: '/build/icons',
-            outputPath: 'icons',
-            prefix: '',
-            inject: true,
-            orientation: 'portrait',
-            favicons: {
-                appName: null,
-                appDescription: null,
-                developerName: null,
-                developerURL: null,
-                theme_color: null,
-                lang: null,
-                start_url: null,
-                icons: {
-                    background: false,
-                    android: false,
-                    appleIcon: false,
-                    appleStartup: false,
-                    coast: false,
-                    favicons: true,
-                    firefox: false,
-                    opengraph: false,
-                    twitter: false,
-                    yandex: false,
-                    windows: false
-                }
-            }
-        })
-    );
 }
 
 const standardConfig = Encore.getWebpackConfig();
@@ -176,64 +114,6 @@ if (Encore.isProduction()) {
         new CompressionPlugin({
             test: /\.(js|css)$/,
             cache: true
-        })
-    );
-    // SW Generation
-    Encore.addPlugin(
-        new WorkboxPlugin.GenerateSW({
-            globDirectory: 'public',
-            clientsClaim: true,
-            skipWaiting: true,
-            globPatterns: [
-                '**/*.{gz,js,css,jpg,JPG,png,ico,otf,eot,ttf,woff,woff2,svg}'
-            ],
-            runtimeCaching: [
-                {
-                    urlPattern: new RegExp('/*(.*)'),
-                    handler: 'StaleWhileRevalidate'
-                },
-                {
-                    urlPattern: new RegExp('https://cdnjs.cloudflare.com/'),
-                    handler: 'StaleWhileRevalidate'
-                }
-            ],
-            swDest: '../sw.js'
-        })
-    );
-    // Generate HTML for Favicons + PWA
-    Encore.addPlugin(
-        new HtmlWebpackPlugin({
-            filename: path.join(__dirname, 'templates/partials/favicons.html.twig'),
-            template: path.join(__dirname, 'templates/faviconsTmpl.html.twig'),
-            inject: true,
-            minify: false,
-            excludeChunks: ['css/main', 'js/main']
-        })
-    );
-// Generate Favicons + PWA stuff
-    Encore.addPlugin(
-        new WebappWebpackPlugin({
-            logo: path.join(__dirname, config.icon.path),
-            cache: true,
-            publicPath: '/build/icons',
-            outputPath: 'icons',
-            prefix: '',
-            inject: true,
-            orientation: 'portrait',
-            favicons: {
-                appName: config.icon.name,
-                appDescription: config.icon.description,
-                developerName: '',
-                developerURL: config.icon.devURL,
-                background: config.icon.backgroundColor,
-                theme_color: config.icon.themeColor,
-                lang: 'de-DE',
-                start_url: '/',
-                icons: {
-                    coast: false,
-                    yandex: false
-                }
-            }
         })
     );
 }
